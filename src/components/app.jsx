e5cd252a899d018c9c919442347863c52ef38eca
Keaton Krieger <bajabuggy313@gmail.com>
2020-03-04 09:39:48 -0800

    Added next card and flip card

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -7,20 +7,25 @@ import Nav from './nav';
 class App extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
       view: 'view-cards',
-      cssBadge: 'view-cards',
-      cards: []
+      cards: [],
+      activeCard: '',
+      arrayIndex: 0
     };
+
     this.setView = this.setView.bind(this);
     this.getView = this.getView.bind(this);
     this.saveCards = this.saveCards.bind(this);
     this.addCards = this.addCards.bind(this);
+    this.setActiveCard = this.setActiveCard.bind(this);
   }
 
   componentDidMount(){
     let cards = localStorage.getItem('flash-cards');
     cards = JSON.parse(cards);
+
     if(cards !== null){
         this.setState({
             cards: cards
@@ -30,19 +35,26 @@ class App extends React.Component {
 
   setView(newView) {
     this.setState({
-      view: newView,
-      cssBadge: newView
+      view: newView
     });
   }
 
   getView() {
     switch (this.state.view) {
       case 'create-cards':
-        return <CreateCards setView={this.setView} addCards={this.addCards}/>;
+        return <CreateCards 
+          setView={this.setView}
+          addCards={this.addCards}
+          />;
       case 'review-cards':
-        return <ReviewCards />;
+        return <ReviewCards 
+          setActiveCard={this.setActiveCard} 
+          activeCard={this.state.activeCard}
+          index={this.state.arrayIndex}
+            />;
       case 'view-cards':
-        return <ViewCards cards={this.state.cards}/>;
+        return <ViewCards 
+          cards={this.state.cards}/>;
       default:
         return null;
     }
@@ -58,6 +70,23 @@ class App extends React.Component {
     this.setState({
         cards: [...this.state.cards, card]
     }, () => {this.saveCards()});
+  }
+
+  setActiveCard(indexProp){
+    let index = indexProp.index
+
+    if(index >= this.state.cards.length){
+        index = 0;
+    } else if (index == -1){
+        index = this.state.cards.length -1
+    } else if (index === undefined){
+      index = 0
+    }
+
+    this.setState({
+      activeCard: this.state.cards[index],
+      arrayIndex: index
+    })
   }
 
   render() {
--- src/components/review-cards.jsx
+++ src/components/review-cards.jsx
@@ -1,8 +1,79 @@
 import React from 'react';
 
 class ReviewCards extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            cardDisplay: "Front"
+        }
+        this.nextCard = this.nextCard.bind(this)
+        this.previousCard = this.previousCard.bind(this)
+        this.componentDidMount = this.componentDidMount.bind(this)
+        this.flipCard = this.flipCard.bind(this);
+    }
+
+    componentDidMount(){
+        let index = this.props.index
+        this.props.setActiveCard({index})
+    }
+
+    nextCard(){
+        let index = this.props.index + 1;
+        this.props.setActiveCard({index});
+    }
+
+    previousCard(){
+        let index = this.props.index - 1;
+        this.props.setActiveCard({index});
+    }
+
+    flipCard(e){
+        console.log('flip card being called')
+        if(this.state.cardDisplay == 'Front'){
+            this.setState({
+                cardDisplay: "Back"
+            })
+        } else {
+            this.setState({
+                cardDisplay: "Front"
+            })
+        }
+    }
+
     render(){
-        return <h1 className="text-center">Review Cards</h1>
+        console.log(this.state.cardDisplay)
+        let text;
+        if(this.state.cardDisplay == 'Front'){
+            let data = this.props.activeCard.question
+            text = <h1 className="text-center mt-5 mb-5">{data}</h1>
+        } else {
+            let data = this.props.activeCard.answer
+            text = <h1 className="text-center mt-5 mb-5">{data}</h1>
+        }
+        return (
+            <div>
+                <div>
+                    <h1 className="text-center">Review Cards</h1>
+                </div>
+                <div className='container p-3 my-3 bg-dark text-white'>
+                    <div className="row align-items-center" onClick={this.flipCard}>
+                        <div className="col-sm" onClick={(event) => {event.stopPropagation()}}>
+                            <div onClick={this.previousCard}>
+                                <span className="fas fa-angle-left fa-3x"></span>
+                            </div>
+                        </div>
+                        <div className="col-sm">
+                            {text}
+                        </div>
+                        <div className="col-sm" onClick={(event) => {event.stopPropagation()}}>
+                            <div onClick={this.nextCard}>
+                                <span className="fas fa-angle-right fa-3x float-right"></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
     }
 }
 
